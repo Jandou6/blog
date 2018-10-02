5a4de89f6918bd8a081e2c65834979d0013ccc62
jandou <809781809@qq.com>
2018-10-02 20:41:15 +0800

    feat: gzip

--- bin/server.js
+++ bin/server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+const compression = require('compression');
 const app = express();
 const PORT = 8080;
-
+app.use(compression());
 app.use(express.static(path.resolve(__dirname,'../dist'), {
   maxAge: '1y',
 }));
