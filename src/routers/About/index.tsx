1127c3ae0314b2a4bb809584a025ef234c0bdf19
jandou <809781809@qq.com>
2018-11-25 16:14:11 +0800

    update: about me

--- src/routers/About/index.tsx
+++ src/routers/About/index.tsx
@@ -2,16 +2,22 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import * as CSSModules from 'react-css-modules';
 const style = require('./style');
-import { Link } from 'react-router-dom';
+import TopNavComponent from '../../components/top_nav';
 @CSSModules(style, {allowMultiple: true})
 class AboutComponent extends React.Component {
   render() {
     return (
       <div styleName="about-page">
-        <h1>Now is in About Page.</h1>
-        <p>
-          You can also click<Link to={'/'}> home page</Link>to try react-router.
-        </p>
+        <TopNavComponent title="About ME"/>
+        <ul styleName="info-wrap">
+          <li>
+            <h4>github:</h4> <a href="https://github.com/Jandou6" target="_brank">https://github.com/Jandou6</a>
+          </li>
+          <li>
+            <h4>ISSUES:</h4>
+            <a href="https://github.com/Jandou6/blog/issues"  target="_brank">https://github.com/Jandou6/blog/issues</a>
+          </li>
+        </ul>
       </div>
     );
   }
